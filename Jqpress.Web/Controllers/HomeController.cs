8625a5d3ac16d3f49e898565e84c60be693e296d
robotbird <robotbird@163.com>
2013-11-13 14:00:12 +0800

    首页模板剩余关键词没完成

--- Jqpress.Web/Controllers/HomeController.cs
+++ Jqpress.Web/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using Jqpress.Framework.Themes;
 using Jqpress.Framework.Web;
 using Jqpress.Blog.Services;
 using Jqpress.Blog.Entity;
+using Jqpress.Blog.Entity.Enum;
+using Jqpress.Blog.Configuration;
 using Jqpress.Web.Models;
 
 namespace Jqpress.Web.Controllers
@@ -16,7 +18,17 @@ namespace Jqpress.Web.Controllers
         public ActionResult Index()
         {
             var model = new IndexModel();
+
+            model.SiteName = BlogConfig.GetSetting().SiteName;
             model.ThemeName = "prowerV5";
+            model.PageTitle = BlogConfig.GetSetting().SiteName;
+            model.MetaKeywords = BlogConfig.GetSetting().MetaKeywords;
+            model.MetaDescription = BlogConfig.GetSetting().MetaDescription;
+            model.NavLinks = LinkService.GetLinkList((int)LinkPosition.Navigation, 1);
+            model.RecentTags = TagService.GetTagList(BlogConfig.GetSetting().SidebarTagCount);
+            model.FooterHtml = BlogConfig.GetSetting().FooterHtml;
+            model.GeneralLinks = LinkService.GetLinkList((int)LinkPosition.General, 1);
+
             ThemeService.InitTheme(0);
 
             const int pageSize = 10;
