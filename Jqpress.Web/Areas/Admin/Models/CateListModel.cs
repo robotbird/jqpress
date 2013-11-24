43c078fb48c84d4207585fa0884358e12577fd3d
robotbird <robotbird@qq.com>
2013-11-25 00:15:07 +0800

    开始分类管理的开发

---
+++ Jqpress.Web/Areas/Admin/Models/CateListModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web;
+using Jqpress.Blog.Entity;
+
+namespace Jqpress.Web.Areas.Admin.Models
+{
+    public class CateListModel
+    {
+        public CateListModel() 
+        {
+            CateSelectItem = new List<SelectListItem>();
+        }
+        /// <summary>
+        /// post's category selectitem
+        /// </summary>
+        public List<SelectListItem> CateSelectItem { get; set; }
+
+        public List<CategoryInfo>
+    }
+}
