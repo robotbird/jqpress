2c1ed615aeeb1d0ff75ee5b64c4e91100d3a8d7c
robotbird <robotbird@qq.com>
2014-03-04 22:07:39 +0800

    完成文件管理器的初步上传删除功能

--- Jqpress.Web/Areas/Admin/Controllers/FilesController.cs
+++ Jqpress.Web/Areas/Admin/Controllers/FilesController.cs
@@ -21,10 +21,6 @@ namespace Jqpress.Web.Areas.Admin.Controllers
 
         private string FileName;
 
-        public ActionResult Index()
-        {
-            return View();
-        }
         /// <summary>
         /// 文件浏览
         /// </summary>
@@ -68,6 +64,56 @@ namespace Jqpress.Web.Areas.Admin.Controllers
             model.PathUrl = GetPathUrl();
             return View(model);
         }
+
+
+        /// <summary>
+        /// 上传保存
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            var file = Request.Files["FileUpload"];
+            string basepath = PressRequest.GetQueryString("path");
+            if (file != null)
+            {
+                if (string.IsNullOrEmpty(urlpath))
+                {
+                    urlpath = rootpath;
+                }
+                var savepath = Server.MapPath(urlpath + "/" + file.FileName);
+                file.SaveAs(savepath);
+            }
+            return RedirectToAction("list", new { path = urlpath });
+        }
+
+        /// <summary>
+        /// 删除路径下相应文件
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DeleteFile()
+        {
+            string path = PressRequest.GetQueryString("path").Trim();
+            string url = PressRequest.GetQueryString("url").Trim();
+            if (System.IO.File.Exists(path) || Directory.Exists(path))
+            {
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.FileInfo file = new System.IO.FileInfo(path);
+                    file.Delete();
+                }
+                else
+                {
+                    if (Directory.Exists(path))
+                    {
+                        DirectoryInfo di = new DirectoryInfo(path);
+                        di.Delete(true);
+                    }
+                }
+            }
+            return RedirectToAction("list", new { path = url });
+        }
+
         public FilesModel GetFilesList(string path)
         {
             path = path.Replace("//", "/");
@@ -131,146 +177,5 @@ namespace Jqpress.Web.Areas.Admin.Controllers
             }
             return pathLink.Replace("//", "");
         }
-
-        #region 以下需要逐步实现的
-        /*
-        /// <summary>
-        /// 图片添加浏览页
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult FilesPicBrowser()
-        {
-            var imgWebService = new FileWebService();
-            int pageIndex = PressRequest.GetQueryInt("page", 1);
-            string basepath ="/"+ PressRequest.GetQueryString("path").Replace("//","");
-            int pageSize = 20;
-
-            if (string.IsNullOrEmpty(urlpath) && string.IsNullOrEmpty(basepath))
-                urlpath = rootpath;
-
-            if (!string.IsNullOrEmpty(basepath))
-                urlpath = basepath;
-
-            var list = imgWebService.GetFilesList(urlpath);
-            var model = new ImageListModel();
-            #region webservice
-            model.FoldList = list.FoldList.Select(fold => new FoldInfo
-            {
-                FoldName = fold.FoldName,
-                FoldPath = fold.FoldPath,
-                FileSystemInfosLength = fold.FileSystemInfosLength
-            }).ToList();
-            model.FileList = list.FileList.Select(f => new FileInfo
-            {
-                FileName = f.FileName,
-                Extension = f.Extension,
-                FileLength = f.FileLength,
-                FilePath = f.FilePath,
-                FileUrl = f.FileUrl
-            }).ToList();
-            #endregion
-            if (model.FileList.Count == 0)
-            {
-                IPagedList<FoldInfo> foldlist = new PagedList<FoldInfo>(model.FoldList, pageIndex - 1, pageSize);
-                model.FoldPageList.LoadPagedList<FoldInfo>(foldlist);
-                model.FoldList = (List<FoldInfo>)foldlist;
-            }
-            string controller = RouteData.Values["controller"].ToString();
-            model.ModelID = Convert.ToInt32(RouteData.Values["modelid"]);
-            string aera = "Admin";
-            model.CurrentAction = "/" + aera + "/" + controller + "/" + "FilesBrowser";
-            model.CurrentAction = model.CurrentAction.Replace("//", "/");
-            model.UserDirectory = new DirectoryInfo(Server.MapPath(urlpath));
-            ViewBag.path = urlpath;
-            ViewBag.UrlPath = System.IO.Path.GetFileName(Request.Url.AbsolutePath);
-            model.PathUrl = GetPathUrl().Replace("//","");
-            return View(model);            
-        }
-        /// <summary>
-        /// 删除路径下相应文件
-        /// add by qiankun 20121009
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult DeleteFile()
-        {
-            var imgWebService = new FileWebService();
-            string paths = PressRequest.GetQueryString("path").Trim();
-            string url=PressRequest.GetQueryString("url").Trim();
-            if (paths.IndexOf("userfiles") >= 0)
-            {
-                paths = paths.Substring(paths.IndexOf("uploadfiles"));
-            }
-            if (paths.IndexOf("uploadfiles")>=0)
-            {
-                paths = paths.Substring(paths.IndexOf("uploadfiles"));                
-            }
-            imgWebService.DeletePic(paths);
-            if (states == 1)
-            {
-                return RedirectToAction("FilesPicBrowser", new { path = url });
-            }
-            else 
-            {
-                return RedirectToAction("", new { path =url  });
-            }
-        
-            }
-        /// <summary>
-        /// 保存
-        /// </summary>
-        /// <returns></returns>
-        [HttpPost]
-        public ActionResult SaveFiles()
-        {
-            var file = Request.Files["FileUpload"];
-            string basepath = PressRequest.GetQueryString("path");
-            //SaveLocalFiles();       
-            SaveWebServiceFiles(file, basepath);
-            if (!string.IsNullOrEmpty(basepath))
-                urlpath = basepath;
-           if ( states== 1)
-            {
-                return RedirectToAction("FilesPicBrowser", new { path = urlpath });
-            }
-           else
-            {
-                return RedirectToAction("FilesBrowser", new { path = urlpath });
-            }
-           
-        }
-        /// <summary>
-        /// 保存本地路径
-        /// </summary>
-        void SaveLocalFiles(HttpPostedFileBase file)
-        {
-            if (file != null)
-            {
-                if (string.IsNullOrEmpty(urlpath))
-                {
-                    urlpath = rootpath;
-                }
-                var savepath = Server.MapPath(urlpath + "/" + file.FileName);
-                file.SaveAs(savepath);
-            }
-        }
-
-        /// <summary>
-        /// 保存到远程服务器
-        /// </summary>
-        void SaveWebServiceFiles(HttpPostedFileBase file,string basepath="")
-        {
-            var imgWebService = new FileWebService();
-            string dir = urlpath+"/";
-            dir = dir.Replace(rootpath, "");
-            if (basepath!="")
-            {
-                dir = dir.Replace(basepath, "");
-            }
-            string filename = file.FileName;
-            var img = new ImageInfo { FileName = filename, Path = dir, ByteImage = FileHelper.HttpPostedFileToByte(file) };
-            imgWebService.UploadImageBase(img, basepath);
-        }
-        */
-        #endregion
     }
 }
